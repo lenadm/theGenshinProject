fed80450cf39a1abbf16b168f605af7cfbd80673
ben richards <lenarichards2234@gmail.com>
2023-09-27 22:21:25 +0100

    end of first days work

---
+++ js/activites.js
@@ -0,0 +1,19 @@
+const activites = [
+    "Dvalin",
+    "Boreas",
+    "Childe",
+    "Azhdaha",
+    "La Signora",
+    "Raiden Shogun",
+    "Balladeer",
+    "Apep",
+    "parametric transformer",
+    "main quest",
+    "dailies",
+    "exploring",
+    "talent books",
+    "weapon materials",
+    "regular bosses",
+    "spiral abyss",
+    "friendship farming"
+]
