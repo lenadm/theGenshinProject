23bb5d2b6ed07a4f8cde2ad972950fe49ab77262
ben richards <lenarichards2234@gmail.com>
2023-09-28 18:01:56 +0100

    autocomplete functionality done

--- js/activites.js
+++ js/activites.js
@@ -1,4 +1,4 @@
-const activites = [
+let activities = [
     "Dvalin",
     "Boreas",
     "Childe",
@@ -17,3 +17,31 @@ const activites = [
     "spiral abyss",
     "friendship farming"
 ]
+
+const objectList = document.querySelector(".object-list");
+const inputBox = document.getElementById("input-bar");
+
+inputBox.onkeyup = function() {
+    let result = [];
+    let input = inputBox.value;
+    if(input.length) {
+        result = activities.filter((keyword)=> {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+    }
+    display(result);
+    if(!result.length){
+        objectList.innerHTML = ''
+    }
+}
+
+function display(result){
+    const content = result.map((list)=> {
+        return "<li onclick=selectInput(this)>" + list + "</li>"
+    });
+    objectList.innerHTML = "<ul>" + content.join('') + "</ul>"
+}
+
+function selectInput(list){
+    inputBox.value = list.innerHTML
+} 
